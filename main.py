d54d9b62f47adbb0da0db79207c0b85e59f28715
Jonathan Chiang <jchiangconan@gmail.com>
2024-03-09 10:20:48 -0500

    good version

--- main.py
+++ main.py
@@ -1,27 +1,30 @@
-from __future__ import annotations
+from typing import List, Union
+import time
 import numpy as np
 import requests
 from bs4 import BeautifulSoup
 import re
-from typing import List
 
 
 class CoilSolver:
     username: str = "username"
     password: str = "password"
+    timer_start: float = time.perf_counter()  # Timer start time for each level computation
 
     def __init__(self):
         with open("login.txt", "r") as file:
             self.username = file.readline().strip()
             self.password = file.readline().strip()
-
+    
     def solve(self):
         board = self.get_board()
         print(board)
+        self.timer_start = time.perf_counter()
         self.solve_board(board)
+        print("Time to solve: %.3fs" % (time.perf_counter() - self.timer_start))
 
     def get_board(self) -> np.ndarray[int] | None:
-        url: str = "http://www.hacker.org/coil?name=" + self.username + "&password=" + self.password
+        url = "http://www.hacker.org/coil?name=" + self.username + "&password=" + self.password
         response = requests.get(url)
         if response.status_code == 200:
             try:
@@ -31,6 +34,9 @@ class CoilSolver:
                 width = int(re.search(r'width = (\d+)', script_tag.string).group(1))
                 height = int(re.search(r'height = (\d+)', script_tag.string).group(1))
                 board = np.zeros((height, width), dtype=int)
+                level = int(re.search(r'Level: (\d+)', response.text).group(1))
+                print(f"Level: {level}, dimensions: {width}x{height}")
+
                 for i, char in enumerate(game_board_str):
                     row, col = i // width, i % width
                     if char == 'X':
@@ -56,32 +62,43 @@ class CoilSolver:
         print("No solution found.")
         self.submit_solution(0, 0, "")
 
-    def solve_board_recursion(self, board: np.ndarray[int], x: int, y: int, path: str) -> tuple[bool, str]:
-        """Given a board and path, it will return (False, "") if the path does not lead to a solution,
-        else return [True, <PATH>] with the correct path
+    def submit_solution(self, start_x: int, start_y: int, solution: str):
+        url: str = ("http://www.hacker.org/coil?name=" + self.username + "&password=" + self.password
+                    + "&x=" + str(start_x) + "&y=" + str(start_y) + "&path=" + solution)
+        # response = requests.post(url, data={"x": start_x, "y": start_y, "path": solution})
+        response = requests.get(url)
+        if response.status_code == 200:
+            pass
+        else:
+            print("There was an error submitting the solution.")
+
+    def solve_board_recursion(self, board: np.ndarray, x: int, y: int, path: str) -> tuple[bool, str]:
+        """Given a board and path, it will return [False, ""] if the path does not lead to a solution or it will return
+        [True, <PATH>] with the correct path
 
         :param board: the current state of the board
         :param x: the current position's x value
         :param y: the current position's y value
         :param path: the current path
-        :return whether the path was successful, and the path it took e.g. "RDLDRU"
+        :return List[0]: whether the path was successful, True = succ
+        List[1]: if a path is successful, the path it took e.g. "RDLDRU"
 
         # >>> cs = CoilSolver()
-        # >>> tempboard = numpy.array([[1, 0, 0],[0, 0, 0],[0, 0, 1]], int)
-        # TODO: Fix this test
+        # >>> cs.boardX = 3
+        # >>> cs.boardY = 3
+        # >>> tempboard = np.array([[1, 0, 0],[0, 0, 0],[0, 0, 1]], int)
+        # >>> cs.solve_board_recursion(tempboard, 1, 0, "")
+        # True, 'RDLDR'
         """
 
-        # if there are multiple cells with only one neighbor then it is impossible to solve
         if not self.neighbors_valid(board, x, y):
             return False, ""
 
-        valid_dirs: [str] = self.can_move(board, x, y)
-        # print("valid dirs", valid_dirs)
+        valid_dirs = self.can_move(board, x, y)
 
-        # if len(valid_dirs) > 0:
-        #     # Adds flood fill efficiency; if the board is unsolvable then halt recursion.
-        #     if self.flood_fill_check(tempboard, x, y, valid_dirs[0]) is False:
-        #         return [False, ""]
+        if len(valid_dirs) > 0:
+            if self.flood_fill_check(board, x, y, valid_dirs[0]) is False:
+                return False, ""
 
         for dirs in valid_dirs:
             newboard = board.copy()
@@ -90,9 +107,9 @@ class CoilSolver:
             if self.check_solved(newboard):
                 return True, path + dirs
 
-            succ, new_path = self.solve_board_recursion(newboard, result[0], result[1], path + dirs) # recurse
-            if succ:
-                return succ, new_path
+            thing = self.solve_board_recursion(newboard, result[0], result[1], path + dirs)  # recurse
+            if thing[0]:
+                return thing
 
         return False, ""
 
@@ -107,9 +124,9 @@ class CoilSolver:
         """
         rows, cols = board.shape
         single_empty_neighbors = 0
-        for y in range(cols):
-            for x in range(rows):
-                if board[y, x] == 0 and (x, y) != (curr_x, curr_y):
+        for y in range(rows):
+            for x in range(cols):
+                if board[y, x] == 0 and abs(curr_y - y) + abs(curr_x - x) > 1:
                     if self.count_num_empty_neighbors(board, x, y) == 1:
                         single_empty_neighbors += 1
                         if single_empty_neighbors > 1:
@@ -129,6 +146,86 @@ class CoilSolver:
             count += 1
         return count
 
+    def flood_fill_check(self, board: np.ndarray, x: int, y: int, direction: str) -> bool:
+        """Uses flood fill starting at a given x, y position to determine if a board is solvable in a given state. If a
+        flood fill cannot fill the whole board it means it cannot be solved. Take in a board, returns False if it is not
+        solvable. Does not mutate tempboard.
+
+        :param board:
+        :param x: current x position
+        :param y: current y position
+        :param direction:
+        :return:
+        """
+        checkboard = board.copy()
+
+        if direction == "U":
+            self.flood_fill(checkboard, x, y - 1)
+        elif direction == "D":
+            self.flood_fill(checkboard, x, y + 1)
+        elif direction == "L":
+            self.flood_fill(checkboard, x - 1, y)
+        elif direction == "R":
+            self.flood_fill(checkboard, x + 1, y)
+        if self.count_board_empty(checkboard) > 0:
+            return False
+        return True
+
+    def flood_fill(self, tempboard: np.ndarray, x: int, y: int) -> None:
+        """Starts flood filling tempboard from a given (<x>, <y>) position.
+        Warning: mutates tempboard
+
+        :param tempboard: the board to be checked
+        :param x: the current x position on the board
+        :param y: the current y position on the board
+        :return:
+
+        # >>> cs = CoilSolver()
+        # >>> cs.boardX = 3
+        # >>> cs.boardY = 3
+        # >>> tempboard = np.array([[1, 0, 0],  [0, 1, 0], [0, 0, 1]], int)
+        # >>> cs.flood_fill(tempboard, 2, 0)
+        # >>> print(tempboard)
+        # [[1 1 1]
+        #  [0 1 1]
+        #  [0 0 1]]
+        """
+        rows, cols = tempboard.shape
+        if tempboard[y][x] == 0:
+            tempboard[y][x] = 1
+            # flood fill upwards
+            if y > 0:
+                self.flood_fill(tempboard, x, y - 1)
+            # flood fill downwards
+            if y < rows - 1:
+                self.flood_fill(tempboard, x, y + 1)
+            # flood fill leftwards
+            if x > 0:
+                self.flood_fill(tempboard, x - 1, y)
+            # flood fill rightwards
+            if x < cols - 1:
+                self.flood_fill(tempboard, x + 1, y)
+
+    def count_board_empty(self, board: np.ndarray) -> int:
+        """Counts how many empty spaces there are left on the board
+
+        :param board: the current state of the board
+        :return:
+
+        # >>> tempboard = np.array([[1, 1, 1],[0, 1, 1],[0, 0, 1]], int)
+        # >>> cs = CoilSolver()
+        # >>> cs.boardX = 3
+        # >>> cs.boardY = 3
+        # >>> cs.count_board_empty(tempboard)
+        # 3
+        """
+        rows, cols = board.shape
+        count = 0
+        for y in range(rows):
+            for x in range(cols):
+                count += board[y][x]
+        return rows * cols - count
+
     def check_solved(self, board: np.ndarray[int]) -> bool:
         return np.all(board == 1)
 
@@ -141,7 +238,9 @@ class CoilSolver:
         :return: str array that represents where valid moves are
 
         # >>> cs = CoilSolver()
-        # >>> tempboard = numpy.array([[1, 0, 0],[0, 0, 0],[0, 0, 1]], int)
+        # >>> cs.boardX = 3
+        # >>> cs.boardY = 3
+        # >>> tempboard = np.array([[1, 0, 0],[0, 0, 0],[0, 0, 1]], int)
         # >>> cs.can_move(tempboard, 1, 0)
         # ['D', 'R']
         """
@@ -159,7 +258,7 @@ class CoilSolver:
 
         return moves
 
-    def move(self, board: np.ndarray, x: int, y: int, direction: str) -> (int, int, np.ndarray):
+    def move(self, board: np.ndarray[int], x: int, y: int, direction: str) -> tuple[int, int, np.ndarray[int]]:
         """Determines how a move affects the state of the board. This mutates tempboard.
 
         :param board: the current state of the board
@@ -170,7 +269,9 @@ class CoilSolver:
         List[1]: the y-value of the position after the move
 
         # >>> cs = CoilSolver()
-        # >>> tempboard = numpy.array([[1, 0, 0],[0, 0, 0],[0, 0, 1]], int)
+        # >>> cs.boardX = 3
+        # >>> cs.boardY = 3
+        # >>> tempboard = np.array([[1, 0, 0],[0, 0, 0],[0, 0, 1]], int)
         # >>> output = cs.move(tempboard, 1, 0, "D")
         # >>> output[0]
         # 1
@@ -206,19 +307,11 @@ class CoilSolver:
 
         return x, y, board
 
-    def submit_solution(self, start_x: int, start_y: int, solution: str):
-        url: str = ("http://www.hacker.org/coil?name=" + self.username + "&password=" + self.password
-                    + "&x=" + str(start_x) + "&y=" + str(start_y) + "&path=" + solution)
-        # response = requests.post(url, data={"x": start_x, "y": start_y, "path": solution})
-        response = requests.get(url)
-        if response.status_code == 200:
-            pass
-        else:
-            print("There was an error submitting the solution.")
-
 
 if __name__ == "__main__":
+    # import doctest
+    # doctest.testmod()
+
     cs = CoilSolver()
     while True:
         cs.solve()
-        # break
